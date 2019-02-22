f6b30e20e01422cd929e32072248292693a10f0c
Unknown <TAdcox009@west-mec.org>
2019-02-21 18:24:52 -0700

    commit 2
    added more code to the calCaption function.

--- lht_calendar.js
+++ lht_calendar.js
@@ -39,6 +39,7 @@ document.getElementById("calendar").innerHTML = createCalendar(thisDay);
 // Function to creat the calendar table
 function createCalendar(calDate) {
       var calendarHTML = "<table id='calendar_table'>";
+      calendarHTML += calCaption(calDate);
       calendarHTML += "</table>";
       return calendarHTML;
 }
@@ -47,4 +48,16 @@ function createCalendar(calDate) {
 function calCaption(calDate) {
       // monthName array contains the list of month names
       var monthName = ["January", "Febuary", "March", "April", "May", "June", "July", "August", "September", "October", "November", "December"];
+
+      //determine the current month
+      var thisMonth = calDate.getMonth();
+
+      //determine the current year
+      var thisYear = calDate.getFullYear();
+
+      //write the caption
+      return "<caption>" + monthName[thisMonth] + " "
+      thisYear + "</caption>";
+
+
 }
